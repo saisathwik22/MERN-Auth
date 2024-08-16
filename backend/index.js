18648b851c6422e66a2958eece368759fd174cfe
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-16 20:43:00 +0530

    database setup handled and connection established.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
 
 const app = express();
 
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
 app.listen(3000, () => {
+  connectDB();
   console.log("Server is running on port 3000");
 });
+
+//
