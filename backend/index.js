b1840a3db0cb5a3647266dbacf3e082e37e5681a
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-17 14:36:41 +0530

    Sending otp through signup and welcome email template configured.

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
 
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -21,3 +21,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     throw new Error(`Error sending verification email: ${error}`);
   }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "5c754fcd-2cea-4295-a447-c93378a57f8c",
+      template_variables: {
+        company_info_name: "Authentication",
+        name: name,
+      },
+    });
+
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
+    throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
